40d9ecdb9a53cc9bd4046f03915ab093165799d5
yakaska <valentin7petr@gmail.com>
2023-09-11 15:07:32 +0300

    Initial commit

---
+++ src/main/java/ru/yakaska/tengen/TengenApplication.java
@@ -0,0 +1,13 @@
+package ru.yakaska.tengen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TengenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TengenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yakaska/tengen/TengenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakaska.tengen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TengenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
